c5243efdda05c3882c41b61b7823a929ff5b9406
Seiffoudine <seiffoudine.mogne@gmail.com>
2022-07-07 18:53:30 +0200

    We change again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app=express();
 app.use(express.static('dist/auto'));
 
 app.get('/*',(req,res)=>{
-res.status(200).sendFile(path.join(__dirname+'dist/auto-copie/index.html'));
+res.status(200).sendFile(path.join(__dirname+'dist/fromfutureang/index.html'));
 });
 
 //LAst change
