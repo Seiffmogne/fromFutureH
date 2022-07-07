f6f50bdf79ee9bc243ebc86ab25fef74fdd74727
Seiffoudine <seiffoudine.mogne@gmail.com>
2022-07-07 20:36:54 +0200

    the final change

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express= require('express');
 const  path  = require('path');
 const app=express();
 
-app.use(express.static('dist/future'));
+app.use(express.static(__dirname +'/dist/from-future-ang'));
 
 app.get('/*',(req,res)=>{
-res.status(200).sendFile(path.join(__dirname+'dist/from-future-ang/index.html'));
+res.status(200).sendFile(path.join(__dirname+'/dist/from-future-ang/index.html'));
 });
 
 //LAst change
