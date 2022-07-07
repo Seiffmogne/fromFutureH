4a0db4121c5a442c5f4d04522e3f0f825e44c47f
Seiffoudine <seiffoudine.mogne@gmail.com>
2022-07-07 18:44:00 +0200

    the last change

---
+++ server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const { path } = require('express/lib/application');
+const app=express();
+
+app.use(express.static('dist/auto'));
+
+app.get('/*',(req,res)=>{
+res.status(200).sendFile(path.join(__dirname+'dist/auto-copie/index.html'));
+});
+
+//LAst change
+
+const port=process.env.PORT || 8080;
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log(`Listening on Port ${port} ...`);
+});
